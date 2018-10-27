65ca92667ca736503ebd89e648547584b8849610
ecot <johnpastor90@gmail.com>
2018-10-27 12:08:30 -0500

    working routes

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var express = require("express");
-
+var path = require('path');
 var app = express();
 var PORT = 3000;
 //----
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+
+//------
   
   app.get("/", function(req, res) {
-    res.send("Welcome to the IndoPak Restaurant!");
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+  app.get("/reservations", function(req, res) {
+    res.sendFile(path.join(__dirname, "indopakaddres.html"));
+  });
+
+  app.get("/reservationlist", function(req, res) {
+    res.sendFile(path.join(__dirname, "indopakdisres.html"));
+  });
+//-----
+
+  // app.get("/", function(req, res) {
+  //   res.send("Welcome to the IndoPak Restaurant!");
+  // });
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
